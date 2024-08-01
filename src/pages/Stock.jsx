138734a23225c0edec0f1870dbab1e3853166afa
Victor-Muniu <victornjoroge2020@gmail.com>
2024-08-01 12:12:55 +0300

    done:

--- src/pages/Stock.jsx
+++ src/pages/Stock.jsx
@@ -11,7 +11,9 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  TextField
+  TextField,
+  Button,
+  Modal
 } from '@mui/material';
 import {
   ResponsiveContainer,
@@ -34,30 +36,32 @@ const Stock = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [filteredStartDate, setFilteredStartDate] = useState('');
   const [filteredEndDate, setFilteredEndDate] = useState('');
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [currentTransfer, setCurrentTransfer] = useState({});
   const role = localStorage.getItem('role');
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const response = await axios.get('https://hotel-backend-1-trhj.onrender.com/transfers');
-        let data = response.data;
+    fetchData();
+  }, []);
 
-        if (role === 'service') {
-          data = data.filter(item => item.group === 'Bar');
-        } else if (role === 'front office' || role=== 'admin') {
-          data = data.filter(item => item.group === 'Curio');
-        } else if (role === 'procurement') {
-          data = data.filter(item => item.group === 'House Keeping' || item.group === 'Internal');
-        }
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://hotel-backend-1-trhj.onrender.com/transfers');
+      let data = response.data;
 
-        setData(data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
+      if (role === 'service') {
+        data = data.filter(item => item.group === 'Bar');
+      } else if (role === 'front office' || role === 'admin') {
+        data = data.filter(item => item.group === 'Curio');
+      } else if (role === 'procurement') {
+        data = data.filter(item => item.group === 'House Keeping' || item.group === 'Internal');
       }
-    };
 
-    getData();
-  }, [role]);
+      setData(data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   const formatDataForCharts = (data) => {
     const months = Array.from({ length: 12 }, (v, k) => dayjs().month(k).format('MMM'));
@@ -96,11 +100,11 @@ const Stock = () => {
   const filteredDataByDate = handleFilterByDate();
 
   const handleStartDateChange = (e) => {
-    setFilteredStartDate(dayjs(e.target.value).startOf('day').toDate()); // Convert dayjs object to Date object
+    setFilteredStartDate(dayjs(e.target.value).startOf('day').toDate());
   };
 
   const handleEndDateChange = (e) => {
-    setFilteredEndDate(dayjs(e.target.value).endOf('day').toDate()); // Convert dayjs object to Date object
+    setFilteredEndDate(dayjs(e.target.value).endOf('day').toDate());
   };
 
   const handleSearchChange = (e) => {
@@ -112,6 +116,26 @@ const Stock = () => {
     item.group.toLowerCase().includes(searchTerm.toLowerCase()) ||
     item.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
+
+  const handleEditClick = (transfer) => {
+    setCurrentTransfer(transfer);
+    setEditModalOpen(true);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setCurrentTransfer({ ...currentTransfer, [name]: value });
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await axios.patch(`https://hotel-backend-1-trhj.onrender.com/transfers/${currentTransfer._id}`, currentTransfer);
+      setData(prevData => prevData.map(item => item._id === response.data._id ? response.data : item));
+      setEditModalOpen(false);
+    } catch (error) {
+      console.error('Error updating transfer:', error);
+    }
+  };
 
   return (
     <Box sx={{ flexGrow: 1, p: 3 }}>
@@ -193,7 +217,7 @@ const Stock = () => {
         <Grid item xs={12}>
           <Paper sx={{ p: 3 }}>
             <Typography variant='h6'>Stock</Typography>
-            <InvoiceTable data={filteredDataBySearch.filter(item => filteredDataByDate.includes(item))} />
+            <InvoiceTable data={filteredDataBySearch.filter(item => filteredDataByDate.includes(item))} onEditClick={handleEditClick} />
           </Paper>
         </Grid>
         <Grid item xs={12}>
@@ -211,28 +235,70 @@ const Stock = () => {
           </Paper>
         </Grid>
       </Grid>
+
+      <Modal open={editModalOpen} onClose={() => setEditModalOpen(false)}>
+        <Box sx={{ p: 3, backgroundColor: 'white', margin: 'auto', marginTop: '10%', width: '50%' }}>
+          <Typography variant="h6" gutterBottom>Edit Transfer</Typography>
+          <TextField
+            label="Name"
+            name="name"
+            value={currentTransfer.name || ''}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Quantity"
+            name="quantity"
+            type="number"
+            value={currentTransfer.quantity || ''}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Price"
+            name="unit_price"
+            type="number"
+            value={currentTransfer.unit_price || ''}
+            onChange={handleInputChange}
+            fullWidth
+            margin="normal"
+          />
+          <Button variant="contained" color="primary" onClick={handleSave}>
+            Save
+          </Button>
+        </Box>
+      </Modal>
     </Box>
   );
 };
+const role = localStorage.getItem('role')
 
-const InvoiceTable = ({ data }) => (
+const InvoiceTable = ({ data, onEditClick }) => (
   <TableContainer component={Paper}>
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
+    <Table>
       <TableHead>
         <TableRow>
           <TableCell>Name</TableCell>
           <TableCell>Group</TableCell>
-          <TableCell>Unit Of Measurement</TableCell>
-          <TableCell>Unit Price</TableCell>
+          <TableCell>Description</TableCell>
+          <TableCell>
+            Price
+          </TableCell>
           <TableCell>Quantity</TableCell>
           <TableCell>Spoilt</TableCell>
           <TableCell>Value</TableCell>
           <TableCell>Date</TableCell>
+          {role === 'admin' &&(
+           <TableCell>Actions</TableCell> 
+          )}
+          
         </TableRow>
       </TableHead>
       <TableBody>
         {data.map((row) => (
-          <TableRow key={row.id}>
+          <TableRow key={row._id}>
             <TableCell>{row.name}</TableCell>
             <TableCell>{row.group}</TableCell>
             <TableCell>{row.description}</TableCell>
@@ -241,6 +307,12 @@ const InvoiceTable = ({ data }) => (
             <TableCell>{row.spoilt}</TableCell>
             <TableCell>{row.value}</TableCell>
             <TableCell>{dayjs(row.date).format('MMM DD, YYYY')}</TableCell>
+            {role === 'admin' &&(
+            <TableCell>
+              <Button variant="contained" color="primary" onClick={() => onEditClick(row)}>Edit</Button>
+            </TableCell>
+            )}
+            
           </TableRow>
         ))}
       </TableBody>
